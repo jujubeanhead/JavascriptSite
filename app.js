7ee374bd441c2a4717fad30db643f715208b4f82
Julie <97917643+jujubeanhead@users.noreply.github.com>
2024-04-14 20:11:59 -0400

    Add files via upload
    moved stuff into app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-async function getData(selected_meal) {
-    var response = await fetch('menu_items.json');   // this is a GET request
+async function getData(selectedMajor) {
+    var response = await fetch('cit5students.json');   // this is a GET request
 
     if(response.ok) {
         var data = await response.json();
 
         // filter data array for the selected meal
-        meal_items = data.filter( (item) => item.meal == selected_meal );  
+        majorData = data.filter( (item) => item.name == selectedMajor );
 
-       var templateText = document.getElementById('menuTemplate').innerHTML;
+       var templateText = document.getElementById('majorTemplate').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
-       compiledHtml = compiledTemplateText({ rows: meal_items })      // apply data to template
-       document.getElementById('menuTable').innerHTML = compiledHtml; 
+       compiledHtml = compiledTemplateText({ rows: majorData })      // apply data to template
+       document.getElementById('majorTable').innerHTML = compiledHtml;
     }
     else {
-       document.querySelector('#menuTable').innerHTML = "There was an error, or menu items not found";
-    }	
- 
+       document.querySelector('#majorTable').innerHTML = "There was an error, or menu items not found";
+    }
+
 }
